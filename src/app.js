92e702c54128bf80c1b69b74bd5f2f59ce0a9daf
Ryan Nolan <ryannolanco@gmail.com>
2024-04-09 15:40:41 -0600

    adding query selector functions

--- src/app.js
+++ src/app.js
@@ -3,15 +3,18 @@ const morgan = require("morgan");
 const app = express();
 
 const sayHello = (req, res, next) => {
-  res.send("Hello!");
+  res.send('Hello!');
   next();
 };
 
 
-
-app.get("/hello", sayHello)
-
 app.use(morgan("dev"));
+app.get('/hello', sayHello)
+
+app.get('/songs', (req, res) => {
+  const title = req.query.title;
+  res.send(title)
+})
 
 
 module.exports = app;
