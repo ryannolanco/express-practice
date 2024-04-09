595a3e0ca51a4899172722f64d2d6e17cdc755a0
Ryan Nolan <ryannolanco@gmail.com>
2024-04-09 17:19:44 -0600

    adding route params

--- src/app.js
+++ src/app.js
@@ -3,13 +3,22 @@ const morgan = require("morgan");
 const app = express();
 
 const sayHello = (req, res, next) => {
-  res.send('Hello!');
-  next();
+  console.log(req.query);
+  const name = req.query.name;
+  const content = name ? `Hello, ${name}!` : 'Hello!';
+  res.send(content)
 };
 
+const saySomething = (req, res) => {
+  const greeting = req.params.greeting;
+  const content = `${greeting}!`;
+  res.send(`${greeting}!`)
+}
 
 app.use(morgan("dev"));
 app.get('/hello', sayHello)
+app.get('/say/:greeting', saySomething)
+
 
 app.get('/songs', (req, res) => {
   const title = req.query.title;
