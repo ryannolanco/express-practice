078a57637c49b54ca847a4d87a276e379d1257da
Ryan Nolan <ryan@Ryans-MacBook-Pro-2.local>
2024-04-08 20:22:00 -0600

    adding changes to app.js and server.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
+
+const sayHello = (req, res, next) => {
+  res.send("Hello!");
+  next();
+};
+
+
+
+app.use(sayHello);
+app.use(morgan("dev"));
+
 
 module.exports = app;
